5440fcc1df84f6c13724b4eb26db4433b4479abc
Farouk Adel <farouk.adel931@gmail.com>
2026-02-04 02:16:40 +0200

    edit Routes for deploy Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code className='text-danger'>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -33,7 +6,7 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/Pte-Task">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
